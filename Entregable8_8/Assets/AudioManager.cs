f1a4535fc6b291356ba888b6a91c5dcc425256b4
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2022-03-15 22:55:30 +0100

    1

---
+++ Entregable8_8/Assets/AudioManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] Audios;
+    private AudioSource source;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        {
+
+        }
+    }
+
+    void Play()
+    { 
+       
+    }
+
+    void Pause()
+    {
+
+    }
+
+    void Shuffle()
+    {
+
+    }
+
+    void Repeat()
+    {
+
+    }
+
+
+
+
+}
