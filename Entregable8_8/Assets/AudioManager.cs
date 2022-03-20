de9155d8098ca642e2b57b6096e708bee17a0ec0
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2022-03-20 23:59:18 +0100

    2

--- Entregable8_8/Assets/AudioManager.cs
+++ Entregable8_8/Assets/AudioManager.cs
@@ -6,45 +6,82 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     public AudioClip[] Audios;
+    public GameObject[] Canciones;
     private AudioSource source;
+    public bool audioispaused = false;
+    private int CurrentSong;
+    private bool SongisShuffled = false;
+    
+  
 
 
     // Start is called before the first frame update
     void Start()
     {
         source = GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
-        {
-
-        }
     }
 
-    void Play()
-    { 
+     public void Play()
+    {
+        if(!audioispaused)
+        {
+            source.PlayOneShot(Audios[0]);
+            Canciones[0].SetActive(true);
+        }
+        else
+        {
+            source.UnPause();
+        }
+        
+    }
+
+    public  void Pause()
+    {
+      source.Pause();
+      audioispaused = true;
+    }
+
+    public void Next()
+
+    {
+        if(SongisShuffled = true)
+        {
+            Shuffle();
+            source.Stop();
+            Canciones[CurrentSong].SetActive(false);
+            source.PlayOneShot(Audios[CurrentSong + 1]);
+            Canciones[CurrentSong + 1].SetActive(true);
+        }
+        else
+        {
+            source.Stop();
+            Canciones[CurrentSong].SetActive(false);
+            source.PlayOneShot(Audios[+1]);
+            Canciones[+1].SetActive(true);
+        }
+  
        
     }
 
-    void Pause()
+    public void Preview()
     {
+        source.Stop();
+        source.PlayOneShot(Audios[CurrentSong - 1]);
+        Canciones[CurrentSong].SetActive(false);
+        Canciones[CurrentSong - 1].SetActive(true);
+    }
+
+    public  void Shuffle()
+    {
+        CurrentSong = Random.Range(0, 5);
+        SongisShuffled = true;
 
     }
 
-    void Shuffle()
+    public  void Repeat()
     {
 
     }
-
-    void Repeat()
-    {
-
-    }
-
-
-
 
 }
